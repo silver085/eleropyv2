75b69bbf0a7e4a1dd09902416e1707b140b0a796
Flavio <fmontesano@gmail.com>
2023-12-12 14:32:26 +0100

    Removed umqttsimple and switched to phaomqtt

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import json
 import time
 from config.config import Config
 from protocol.handler import Handler
-from umqttsimple import MQTTClient
-
+import paho.mqtt.client as mqtt
 
 from radio.radio import Radio
 
@@ -58,33 +57,38 @@ def ext_handler():
                 # radio.raw_transmit(radiomessage, 10)
 
     except OSError as e:
-        restart_and_reconnect()
+        print(f"Error occurred: {e}")
 
 
-def sub_cb(topic, msg):
-    str_topic = topic.decode()
-    destination_str = str_topic.replace("home/elero/action/", "")
+def sub_cb(client, userdata,  msg):
+    str_topic = msg.topic
+    str_payload = msg.payload.decode()
+    destination_str = str_topic.replace("elero/action/", "")
     destination = destination_str.split("/")
-    radiomessage = protocol_handler.buildMsg(destination[0], destination[1], msg.decode())
+    radiomessage = protocol_handler.buildMsg(destination[0], destination[1], str_payload)
     print(radiomessage)
     radio.raw_transmit(radiomessage, 3)
 
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    print(f"Userdata: {userdata} - flags: {flags}")
+    client.subscribe(base_topic + "action/#")
+    print(f"Subscribed to {base_topic} + action/#")
+    client.publish(topic=availability_topic, payload="online", retain=False)
+    print(f"Published alive to {availability_topic}")
+
 def connect_and_subscribe(client_name, mqtt_server, mqtt_port, topic_sub):
     print(f"Connecting to broker {mqtt_server}...")
-    client = MQTTClient(client_name, mqtt_server, user="esp_pub", password="esp_pub", port=mqtt_port, keepalive=60,
-                        ssl=False)
-    client.set_callback(sub_cb)
-    client.connect()
-    client.subscribe(base_topic + "action/#")
-    print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
-    client.publish(topic=availability_topic, msg="online", retain=False)
+    client = mqtt.Client()
+    client.username_pw_set(username="esp_pub", password="esp_pub")
+    client.on_message = sub_cb
+    client.on_connect = on_connect
+    client.connect(host=mqtt_server, port=mqtt_port)
+
+    print(f"Connecting to... ({mqtt_server}:{mqtt_port}")
     return client
 
-
-def restart_and_reconnect():
-    print('Failed to connect to MQTT broker. Reconnecting...')
-    time.sleep(10)
 
 
 try:
@@ -100,9 +104,10 @@ try:
     client = connect_and_subscribe(mqtt_server=mqtt_server, mqtt_port=mqtt_port, client_name=mqtt_client_name,
                                    topic_sub=mqtt_topic)
     print("OK!!")
-    radio = Radio(radio_config=config.get_config()["radio"], radio_wiring=config.get_config()["wiring"])
-    protocol_handler = Handler(addresses=config.get_config()["addresses"], debug=config.get_config()["debug"],
-                               autodiscovery_callback=on_new_blind_discovery)
-    radio.client_loop(callback=on_receive_data, svc_handler=ext_handler)
+    #radio = Radio(radio_config=config.get_config()["radio"], radio_wiring=config.get_config()["wiring"])
+    #protocol_handler = Handler(addresses=config.get_config()["addresses"], debug=config.get_config()["debug"],
+    #                           autodiscovery_callback=on_new_blind_discovery)
+    #radio.client_loop(callback=on_receive_data, svc_handler=ext_handler)
+    client.loop_forever()
 except OSError as e:
-    restart_and_reconnect()
+    print(f"Error occurred: {e}")
